23798ed005e0f0bd2a19d74fee06ef6cf598cae0
Andy McGill <andymcg7@googlemail.com>
2022-05-23 10:37:54 +0100

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.diffplug.spotless") version "5.13.0"
+	id("com.adarshr.test-logger") version "3.0.0"
+	id("org.owasp.dependencycheck") version "6.2.2"
+	id("com.github.ben-manes.versions") version "0.39.0"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.allopen") version "1.5.20"
+	kotlin("plugin.jpa") version "1.5.20"
+	kotlin("plugin.noarg") version "1.5.20"
 }
 
 group = "com.andymcg"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -21,6 +28,18 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
 }
 
 tasks.withType<KotlinCompile> {
