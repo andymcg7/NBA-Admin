32e2ade6041cb9a4a4634574616a362ff056165d
Andy McGill <andymcg7@googlemail.com>
2022-05-23 17:06:50 +0100

    Added socks proxy

--- src/main/kotlin/com/andymcg/northumberlandbadmintonadmin/NorthumberlandBadmintonAdminApplication.kt
+++ src/main/kotlin/com/andymcg/northumberlandbadmintonadmin/NorthumberlandBadmintonAdminApplication.kt
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import java.net.Authenticator
+import java.net.PasswordAuthentication
+import java.net.URL
+
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -12,10 +16,27 @@ class NorthumberlandBadmintonAdminApplication {
 	companion object {
 		@JvmStatic fun main(args: Array<String>) {
 			SpringApplication.run(NorthumberlandBadmintonAdminApplication::class.java, *args)
+			val fixie = URL(System.getenv("FIXIE_SOCKS_HOST"))
+			val fixieUserInfo: List<String> = fixie.userInfo.split(':')
+			val fixieUser = fixieUserInfo[0]
+			val fixiePassword = fixieUserInfo[1]
+			System.setProperty("socksProxyHost", fixie.host)
+			Authenticator.setDefault(ProxyAuthenticator(fixieUser, fixiePassword))
 		}
 	}
 }
 
 object Uris {
 	const val root = ""
+}
+
+class ProxyAuthenticator constructor(user: String, password: String) : Authenticator() {
+	private val passwordAuthentication: PasswordAuthentication
+	override fun getPasswordAuthentication(): PasswordAuthentication {
+		return passwordAuthentication
+	}
+
+	init {
+		passwordAuthentication = PasswordAuthentication(user, password.toCharArray())
+	}
 }
