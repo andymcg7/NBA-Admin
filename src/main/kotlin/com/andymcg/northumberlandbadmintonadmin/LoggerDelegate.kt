e4a235dc3974456a76b93d4cc04942d510b11d81
Andy McGill <andymcg7@googlemail.com>
2022-05-24 10:07:43 +0100

    proxy tidy up and logging

---
+++ src/main/kotlin/com/andymcg/northumberlandbadmintonadmin/LoggerDelegate.kt
@@ -0,0 +1,18 @@
+package com.andymcg.northumberlandbadmintonadmin
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.companionObject
+
+class LoggerDelegate<in R : Any> : ReadOnlyProperty<R, Logger> {
+
+    override fun getValue(thisRef: R, property: KProperty<*>): Logger =
+        LoggerFactory.getLogger(unwrapCompanionObject(thisRef.javaClass))
+
+    private fun <T : Any> unwrapCompanionObject(javaClass: Class<T>): Class<*> {
+        return javaClass.enclosingClass?.takeIf { it.kotlin.companionObject?.java == javaClass }
+            ?: javaClass
+    }
+}
--- src/main/kotlin/com/andymcg/northumberlandbadmintonadmin/NorthumberlandBadmintonAdminApplication.kt
+++ src/main/kotlin/com/andymcg/northumberlandbadmintonadmin/NorthumberlandBadmintonAdminApplication.kt
@@ -1,12 +1,17 @@
 package com.andymcg.northumberlandbadmintonadmin
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
 import java.net.Authenticator
 import java.net.PasswordAuthentication
 import java.net.URL
+import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
@@ -16,15 +21,34 @@ class NorthumberlandBadmintonAdminApplication {
 	companion object {
 		@JvmStatic fun main(args: Array<String>) {
 			SpringApplication.run(NorthumberlandBadmintonAdminApplication::class.java, *args)
+		}
+
+		val logger by LoggerDelegate()
+	}
+
+	@Autowired
+	lateinit var env: Environment
+
+	@PostConstruct
+	fun init() {
+		//			fixie:qAVBUcybILFYwxk@speedway.usefixie.com:1080
+		if (isProfileActive("heroku", env.activeProfiles.toList())) {
 			val fixie = System.getenv("FIXIE_SOCKS_HOST")
 			val parts = fixie.split("@")
 			val fixieUserInfo: List<String> = parts[0].split(':')
 			val fixieUser = fixieUserInfo[0]
 			val fixiePassword = fixieUserInfo[1]
-			System.setProperty("socksProxyHost", parts[1])
+			val fixieProxy = parts[1].split(":")
+			System.setProperty("socksProxyHost", fixieProxy[0])
+			System.setProperty("socksProxyPort", fixieProxy[1])
+			logger.info("fixieProxy: $fixieProxy[0]")
+			logger.info("fixiePport: $fixieProxy[1]")
 			Authenticator.setDefault(ProxyAuthenticator(fixieUser, fixiePassword))
 		}
 	}
+
+	private fun isProfileActive(profile: String, profiles: List<String>): Boolean =
+		profiles.contains(profile)
 }
 
 object Uris {
